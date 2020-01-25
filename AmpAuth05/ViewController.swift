86585c38897ab4d7cef7cf9433f7e82b208d9e59
Henry G Lu <henrywlu@gmail.com>
2020-01-24 17:47:42 -0800

    Initial Commit

---
+++ AmpAuth05/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmpAuth05
+//
+//  Created by Google3 on 1/24/20.
+//  Copyright © 2020 locust communications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
